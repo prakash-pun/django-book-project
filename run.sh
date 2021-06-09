00660c116b4b234f62803700000159f2d256adb0
prakash-pun7 <punbishal777@gmail.com>
2021-06-09 13:43:03 +0545

    initial commit

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# cd backend/
+python3 manage.py makemigrations users books orders pages
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+python3 manage.py collectstatic --noinput
+
+gunicorn bookstore_project.wsgi -b 0.0.0.0:8000
