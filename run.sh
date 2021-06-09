700b47aed4db238788007902c88a9c4fc5c46d09
Prakash Pun <62987311+prakash-pun7@users.noreply.github.com>
2021-06-09 14:59:19 +0545

    update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # cd backend/
-python3 manage.py makemigrations users books orders pages
-python3 manage.py makemigrations
-python3 manage.py migrate
 
-python3 manage.py collectstatic --noinput
+docker-compose up -d --build
+# python3 manage.py makemigrations users books orders pages
+# python3 manage.py makemigrations
+# python3 manage.py migrate
 
-gunicorn bookstore_project.wsgi -b 0.0.0.0:8000
+# python3 manage.py collectstatic --noinput
+
+# gunicorn bookstore_project.wsgi -b 0.0.0.0:8000
