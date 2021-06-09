dc16e9952c56e4968e186a83b9185e9370070c8e
Prakash Pun <62987311+prakash-pun7@users.noreply.github.com>
2021-06-09 20:56:39 +0545

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-pkill -f uwsgi -9
-cd /home/eversoft/microstay/
-source venv/bin/activate
-export ENV_PATH=dev.env
-cd backend/microstay
-git pull
-pip install -r requirements.txt
-python manage.py makemigrations users books orders pages
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput
+cd ..
+# pip install -r requirements.txt
+# python manage.py makemigrations users books orders pages
+# python manage.py makemigrations
+# python manage.py migrate
+# python manage.py collectstatic --noinput
 docker-compose up --build -d
 docker image prune -a --filter "until=24h" -f
-
 exit
