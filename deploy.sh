e15e5b02f5e801697ad6d8f89a5836310158f5e7
Prakash Pun <62987311+prakash-pun7@users.noreply.github.com>
2021-06-10 14:40:55 +0545

    file update

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,15 @@ pkill -f uwsgi -9
 
 mkdir django-book-project
 cd ./django-book-project
-git clone https://github.com/prakash-pun7/django-book-project.git
+git clone https://github.com/prakash-pun7/django-book-project.git .
 
-pipenv shell
-pip install -r requirements.txt
+# pipenv shell
+# pip install -r requirements.txt
 
-python manage.py makemigrations users books orders pages
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput
+# python manage.py makemigrations users books orders pages
+# python manage.py makemigrations
+# python manage.py migrate
+# python manage.py collectstatic --noinput
 docker-compose up --build -d
 docker image prune -a --filter "until=24h" -f
 exit
