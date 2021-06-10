6718bbdde1299f4e990b26f7f42b024ec753f841
Prakash Pun <62987311+prakash-pun7@users.noreply.github.com>
2021-06-10 14:52:04 +0545

    update file

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@
 
 mkdir django-book-project
 cd ./django-book-project
-git clone https://github.com/prakash-pun7/django-book-project.git .
+git clone https://github.com/prakash-pun7/django-book-project .
+
+git pull origin master
 
 docker-compose up -d --build
 
